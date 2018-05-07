6d0a7da080943304f68dd6ab44f4c5f60443bbeb
rishabh <rishabh.moudgil@datadoghq.com>
2018-05-07 15:37:35 -0400

    Update license header, add integration to constants.py

--- couchbase/datadog_checks/couchbase/couchbase.py
+++ couchbase/datadog_checks/couchbase/couchbase.py
@@ -1,7 +1,7 @@
-# (C) Datadog, Inc. 2013-2017
+# (C) Datadog, Inc. 2018
 # (C) Justin Slattery <Justin.Slattery@fzysqr.com> 2013
 # All rights reserved
-# Licensed under Simplified BSD License (see LICENSE)
+# Licensed under a 3-clause BSD style license (see LICENSE)
 
 # stdlib
 import re
